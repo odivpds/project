b814b69e78b37a97cf490bbf616ea9b531cb0379
agusprana <odhivpds01@gmail.com>
2026-07-13 16:51:05 +0800

    Add project files

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * KONFIGURASI CLIENT AFFILIATE
+ * File ini TIDAK AKAN DIACAK, sehingga Client Anda bisa mengedit ini di Github.
+ */
+const CONFIG = {
+    // URL API Pusat (Server Utama Anda) : API_BASE_URL: 'https://api.getfiles.my.id',
+    API_BASE_URL: 'https://api.getfiles.my.id',
+
+    // URL Direct Link Pop-under Milik Client (Kosongkan jika menggunakan script di index.html)
+    CLIENT_POPUNDER_URL: ''
+};

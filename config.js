3737c7badd68129ad02745813710ef97628eb14b
agusprana <odhivpds01@gmail.com>
2026-07-21 21:07:57 +0800

    change database

--- config.js
+++ config.js
@@ -7,5 +7,12 @@ const CONFIG = {
     API_BASE_URL: 'https://api.getfiles.my.id',
 
     // URL Direct Link Pop-under Milik Client (Kosongkan jika menggunakan script di index.html)
-    CLIENT_POPUNDER_URL: ''
+    CLIENT_POPUNDER_URL: '',
+
+    // URL CDN (Pull Zone) dari Bunny Storage Anda (Kosongkan jika masih pakai Stream lama)
+    // Contoh: 'cdn.domainanda.com' atau 'nama-zone.b-cdn.net'
+    BUNNY_PULL_ZONE_URL: 'getfiles.b-cdn.net',
+
+    // URL CDN Cloudflare R2 (Baru)
+    R2_PUBLIC_URL: 'pub-1880d72cd0f343999d3197d949ff8213.r2.dev'
 };
